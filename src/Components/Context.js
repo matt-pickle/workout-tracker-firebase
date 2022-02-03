22e5d89841699f19e8b8e0c1aab00438031ba3bf
Matt Pickle <mpickle86@gmail.com>
2022-02-03 23:35:04 +0000

    Refactor Context to get user info from firebase

--- src/Components/Context.js
+++ src/Components/Context.js
@@ -1,61 +1,33 @@
 import React, {useState, useEffect} from "react";
+import * as firebase from "firebase";
 const Context = React.createContext();
 
 function ContextProvider(props) {
-  const [user, setUser] = useState("");
-  const [workoutHistory, setWorkoutHistory] = useState([]);
-  const [weightHistory, setWeightHistory] = useState([]);
-  const [forceUpdate, setForceUpdate] = useState(true);
+  const [userObj, setUserObj] = useState(null);
+  const [userRef, setUserRef] = useState(null);
 
-  //Gets username from web token
   useEffect(() => {
-    fetch("/user/getUser")
-      .then(res => {
-        if (!res.ok) {
-          res.text().then(text => {
-            console.error(text);
-          });
-        } else {
-          res.text().then(text => {
-            setUser(text);
-            //Gets current user's workoutHistory from database
-            fetch(`/workout/getHistory?user=${text}`)
-              .then(res => {
-                if (!res.ok) {
-                  res.text().then(text => {
-                    console.error(text);
-                  });
-                } else {
-                  res.text().then(text => {
-                    setWorkoutHistory(JSON.parse(text));
-                  });
-                }
-              });
-            //Gets current user's weightHistory from database
-            fetch(`/weight/getHistory?user=${text}`)
-              .then(res => {
-                if (!res.ok) {
-                  res.text().then(text => {
-                    console.error(text);
-                  });
-                } else {
-                  res.text().then(text => {
-                    setWeightHistory(JSON.parse(text));
-                  });
-                }
-              });
-          });
-        }
-      });
-  }, [forceUpdate]);
-  
-  //Forces rerender of Context
-  function updateContext() {
-    setForceUpdate(prev => !prev);
-  }
+    const unsubscribe = firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        const newUserRef = firebase.firestore().collection("users").doc(user.uid);
+        setUserRef(newUserRef);
+        newUserRef.get()
+        .then(doc => {
+          const newUserObj = doc.data();
+          setUserObj(newUserObj);
+        })
+        .catch(err => {
+          alert("Error!", err.message);
+        });          
+      } else {
+        setUserObj(null);
+      }
+    });
+    return () => unsubscribe();
+  }, []);
 
   return (
-    <Context.Provider value={{user: user, workoutHistory: workoutHistory, weightHistory: weightHistory, updateContext: updateContext}}>
+    <Context.Provider value={{userObj: userObj, userRef: userRef}}>
       {props.children}
     </Context.Provider>
   )
