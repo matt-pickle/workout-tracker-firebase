1891a8d49f20c86f89b70dc6ac5c23334a7e40e7
Matt Pickle <mpickle86@gmail.com>
2022-02-06 20:19:11 +0000

    Refactor ProtectedRoute to use getAuth()

--- src/Components/ProtectedRoute.js
+++ src/Components/ProtectedRoute.js
@@ -1,22 +1,9 @@
-import React, {useState, useEffect} from "react";
+import React from "react";
 import {Route, Redirect} from "react-router-dom";
-import {getAuth, onAuthStateChanged} from "firebase/auth";
+import {getAuth} from "firebase/auth";
 
 function ProtectedRoute(props) {
-  const [isAuth, setIsAuth] = useState(true);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(getAuth(), user => {
-      if (user) {
-        setIsAuth(true);        
-      } else {
-        setIsAuth(false);
-      }
-    });
-    return () => unsubscribe();
-  }, []);
-
-  return isAuth ? <Route {...props} /> : <Redirect to="/login" />;
+  return getAuth().currentUser ? <Route {...props} /> : <Redirect to="/login" />;
 }
 
 export default ProtectedRoute;
